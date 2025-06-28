a5a90f06a27fdcb31e8f7e3434017858f673e415
Hema-10 <151361997+Hema-10@users.noreply.github.com>
2025-06-28 20:36:55 +0530

    Create Anagram Checker.java

---
+++ 19.06.2025/Anagram Checker.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String s1 = "listen", s2 = "silent";
+        char[] a = s1.toCharArray(), b = s2.toCharArray();
+        Arrays.sort(a); Arrays.sort(b);
+        System.out.println(Arrays.equals(a, b));
+    }
+}
